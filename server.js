84f24e4b7dcb428bb7053a65e1b9f410a490dbef
Sage Watterworth <watter.sage@gmail.com>
2022-04-30 22:25:01 -0400

    add GET budget and CREATE budget

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.use(cookieParser());
 //         console.log(communication)
 //         console.log(total)
 
+
+//POST CREATE EXPENSES
 app.post('/createExpenses', (req, res) => {
   console.log('hey from server');
 
@@ -51,13 +53,9 @@ app.post('/createExpenses', (req, res) => {
 
   console.log(req.body);
 
-const q = 'INSERT INTO expenses_table (userid,budgetid,groceries,restaurant,barcafe,rent,utilities,insurance,fuel,entertaiment,communication,totalexpenses) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)
+const q = 'INSERT INTO expenses_table (userid,budgetid,groceries,restaurant,barcafe,rent,utilities,insurance,fuel,entertaiment,communication,totalexpenses) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12)'
 
-
-
-});
-
-await db.query(q,[userID,id,groceries,restaurant,barcafe,rent,utilities,insurance,fuel,entertaiment,communication,total],(err,data) => {
+db.query(q,[userID,id,groceries,restaurant,barcafe,rent,utilities,insurance,fuel,entertaiment,communication,total],(err,data) => {
 
   if(err){
 console.log('failed to add to database',err);
@@ -69,6 +67,74 @@ return;
 
 
 })
+
+});
+
+
+
+//POST CREATE BUDGET
+app.post('/createBudget', (req, res) => {
+  console.log('hey from server');
+
+const {
+userId,
+id,
+budgetName,
+periodDate,
+startDate,
+endDate,
+totalAmountAllocated
+}  = req.body;
+
+const q = 'INSERT INTO budget_table (userid,budgetid,budgetname,perioddate,startdate,enddate,totalamountallocated) VALUES ($1,$2,$3,$4,$5,$6,$7)'
+
+db.query(q,[userID,id,budgetName,periodDate,startDate,endDate,totalAmountAllocated],(err,data) => {
+
+  if(err){
+console.log('failed to add to database',err);
+return;
+  }
+
+  console.log(data);
+
+
+
+})
+
+});
+
+
+//GET BUDGET (EXPENSES)
+app.get('/budget/:budgetId', async (req, res) => {
+  console.log('hey from server');
+
+  const {budgetId} = req.params;
+  console.log(req.params)
+
+const q = `SELECT * FROM expenses_table WHERE budgetid = ${budgetId}`;
+
+  console.log(q)
+
+await db.query(q,(err,data) => {
+
+  if(err){
+console.log('failed to add to database',err);
+return;
+  }
+
+res.json ({data: data.rows[0]});
+
+console.log(data.rows[0]);
+
+
+})
+
+});
+
+
+
+
+
 
 app.listen(port, (req, res) => {
   console.log(`server is listening on port ${port}`);
