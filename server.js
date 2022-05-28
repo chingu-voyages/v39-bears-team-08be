bb3bdcab0cba8b36af293d0a072f3a499bd42119
Luis Castillo <mr.luiscastillo@gmail.com>
2022-05-28 15:00:15 -0400

    add cors allowes

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ const { query } = require('express');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
+app.use(cors({
+  origin: "*",  
+  })
+);
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
